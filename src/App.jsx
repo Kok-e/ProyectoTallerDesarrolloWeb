b97337095cb56a3fb0f17c926d96d51c6eef5bdc
Kok-e <118061358+Kok-e@users.noreply.github.com>
2023-04-28 22:46:04 -0400

    punto 1 y 3 listos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Box, Typography, Card, CardMedia, CardContent, Button, Badge } from '@mui/material';
-
+import { useQuery } from '@tanstack/react-query'
+import { Grid, Box, Typography, Card, CardMedia, CardContent, Button, Badge, LinearProgress } from '@mui/material';
 
 function App() {
   const [image, setImage] = useState(null);
@@ -9,32 +9,29 @@ function App() {
   const [rightImages, setRightImages] = useState([]);
   const [dogName, setDogName] = useState('');
   const [isDisabled, setIsDisabled] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [prevDecision, setPrevDecision] = useState(null);
 
-
-  const generateDogName = () => {
-    const characters = 'abcdefghijklmnopqrstuvwxyz';
-    let result = '';
-    for (let i = 0; i < 6; i++) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-  }
+  const fetchDogName = async () => {
+    const response = await fetch("https://nombre-de-perros-default-rtdb.firebaseio.com/perros/.json");
+    const data = await response.json();
+    const names = Object.keys(data);
+    return names[Math.floor(Math.random() * names.length)];
+  };
 
   const fetchNewImage = () => {
     setIsDisabled(true);
-    setLoading(true);
+    setIsLoading(true);
     fetch('https://dog.ceo/api/breeds/image/random')
       .then(response => response.json())
-      .then(data => {
+      .then(async data => {
         setIsDisabled(false);
-        setLoading(false);
+        setIsLoading(false);
         setImage(data.message);
         setIsAccepted(false);
-        setDogName(generateDogName());
+        setDogName(await fetchDogName());
       });
-  }
+  };
 
   const handleAccept = () => {
     setIsAccepted(true);
@@ -61,7 +58,6 @@ function App() {
     setPrevDecision(null);
   }
 
-
   useEffect(() => {
     fetchNewImage();
   }, []);
@@ -70,26 +66,6 @@ function App() {
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }} >
       <img src="..\..\img\titulo.png" alt="" srcset="" />
       <Grid container spacing={2}>
-        <Grid item xs={4}>
-          {leftImages.map((leftImage, index) => (
-            <Box key={`leftImage-${index}`} p={9} mb={0} sx={{ position: 'relative' }}>
-              <Card sx={{ maxWidth: 400, borderRadius: 10, boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)', transition: 'transform 0.3s ease-in-out', '&:hover': { transform: 'scale(1.05)' } }}>
-                <CardMedia component="img" sx={{ maxWidth: '100%', height: 'auto', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} image={leftImage.image} alt="Perro" />
-                <CardContent sx={{ backgroundColor: '#f7f7f7' }}>
-                  <Typography gutterBottom variant="h6" component="div" sx={{ fontWeight: 'bold', color: '#333' }}>
-                    {leftImage.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary" sx={{ color: '#777' }}>
-                    {leftImage.description}
-                  </Typography>
-                </CardContent>
-              </Card>
-              <Box sx={{ position: 'absolute', top: 0, right: 0, backgroundColor: 'red', color: 'white', borderRadius: '0 0 0 20px', padding: '4px 8px', fontWeight: 'bold', fontSize: '12px' }}>
-                Rechazado
-              </Box>
-            </Box>
-          ))}
-        </Grid>
         <Grid item xs={4}>
           {image && (
             <Box p={2}>
@@ -106,16 +82,15 @@ function App() {
                     <Button disabled={isDisabled} variant="contained" color="success" onClick={handleAccept} sx={{ borderRadius: '20px', textTransform: 'none' }}>
                       Aceptar
                     </Button>
-                    {loading && <Typography>Cargando...</Typography>}
                     <Button disabled={isDisabled} variant="contained" color="error" onClick={handleReject} sx={{ borderRadius: '20px', textTransform: 'none' }}>
                       Rechazar
                     </Button>
-                    {loading && <Typography>Cargando...</Typography>}
                     <Button disabled={!prevDecision} variant="contained" onClick={handleUndo} sx={{ borderRadius: '20px', textTransform: 'none' }}>
                       Deshacer
                     </Button>
                   </Box>
                 </CardContent>
+              {isLoading && <LinearProgress />}
               </Card>
             </Box>
           )}
@@ -133,6 +108,26 @@ function App() {
               </Card>
               <Box sx={{ position: 'absolute', top: 0, right: 0, backgroundColor: 'green', color: 'white', borderRadius: '0 0 0 20px', padding: '4px 8px', fontWeight: 'bold', fontSize: '12px' }}>
                 Aceptado
+              </Box>
+            </Box>
+          ))}
+        </Grid>
+        <Grid item xs={4}>
+          {leftImages.map((leftImage, index) => (
+            <Box key={`leftImage-${index}`} p={9} mb={0} sx={{ position: 'relative' }}>
+              <Card sx={{ maxWidth: 400, borderRadius: 10, boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)', transition: 'transform 0.3s ease-in-out', '&:hover': { transform: 'scale(1.05)' } }}>
+                <CardMedia component="img" sx={{ maxWidth: '100%', height: 'auto', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} image={leftImage.image} alt="Perro" />
+                <CardContent sx={{ backgroundColor: '#f7f7f7' }}>
+                  <Typography gutterBottom variant="h6" component="div" sx={{ fontWeight: 'bold', color: '#333' }}>
+                    {leftImage.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" sx={{ color: '#777' }}>
+                    {leftImage.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+              <Box sx={{ position: 'absolute', top: 0, right: 0, backgroundColor: 'red', color: 'white', borderRadius: '0 0 0 20px', padding: '4px 8px', fontWeight: 'bold', fontSize: '12px' }}>
+                Rechazado
               </Box>
             </Box>
           ))}
